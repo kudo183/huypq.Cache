5a33f867914eedf3e4586bb04b562f8814fbf9e0
kudo183@gmail.com <kudo183@gmail.com>
2017-03-25 18:16:24 +0700

    - init huypq.Cache.xproj

---
+++ src/huypq.Cache/RamCache.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+
+namespace huypq.Cache
+{
+    public class RamCache<KeyType, DataType> where DataType : class, new()
+    {
+        class CacheData
+        {
+            public DataType Data { get; set; }
+            public System.IO.MemoryStream DataAsProtoBufStream { get; set; }
+            public int HitCount { get; set; }
+            public long LastAccessTime { get; set; }
+        }
+
+        private static readonly RamCache<KeyType, DataType> _instance = new RamCache<KeyType, DataType>();
+
+        public static RamCache<KeyType, DataType> Instance
+        {
+            get { return _instance; }
+        }
+
+        public void Init(int maxItem)
+        {
+            _maxItem = maxItem;
+            _cache = new Dictionary<KeyType, CacheData>(_maxItem);
+        }
+
+        private int _maxItem;
+
+        private Dictionary<KeyType, CacheData> _cache;
+
+        public DataType Get(KeyType key)
+        {
+            CacheData data;
+            if (_cache.TryGetValue(key, out data) == false)
+            {
+                return null;
+            }
+
+            data.HitCount = data.HitCount + 1;
+            data.LastAccessTime = System.DateTime.Now.Ticks;
+            data.DataAsProtoBufStream.Position = 0;
+            return ProtoBuf.Serializer.Deserialize<DataType>(data.DataAsProtoBufStream);
+        }
+
+        public void AddOrUpdate(KeyType key, DataType data)
+        {
+            CacheData cacheData;
+            if (_cache.TryGetValue(key, out cacheData) == false)
+            {
+                if (_cache.Count == _maxItem)
+                {
+                    long min = long.MaxValue;
+                    KeyType keyOfMinHitItem = default(KeyType);
+                    foreach (var item in _cache)
+                    {
+                        //if (item.Value.HitCount < min)
+                        //{
+                        //    min = item.Value.HitCount;
+                        //    keyOfMinHitItem = item.Key;
+                        //}
+                        if (item.Value.LastAccessTime < min)
+                        {
+                            min = item.Value.LastAccessTime;
+                            keyOfMinHitItem = item.Key;
+                        }
+                    }
+                    _cache.Remove(keyOfMinHitItem);
+                }
+                cacheData = new CacheData()
+                {
+                    Data = data,
+                    HitCount = 0,
+                    LastAccessTime = System.DateTime.Now.Ticks,
+                    DataAsProtoBufStream = new System.IO.MemoryStream()
+                };
+                ProtoBuf.Serializer.Serialize(cacheData.DataAsProtoBufStream, data);
+                _cache.Add(key, cacheData);
+            }
+            else
+            {
+                cacheData.Data = data;
+                cacheData.LastAccessTime = System.DateTime.Now.Ticks;
+                ProtoBuf.Serializer.Serialize(cacheData.DataAsProtoBufStream, data);
+            }
+        }
+
+        public void Clear()
+        {
+            _cache.Clear();
+        }
+
+        public string GetCacheInfo()
+        {
+            var sb = new System.Text.StringBuilder();
+            sb.AppendLine("MaxItem: " + _maxItem);
+            sb.AppendLine("Item count: " + _cache.Count);
+            foreach (var item in _cache)
+            {
+                sb.AppendLine(string.Format("hitcount: {0} key: {1}", item.Value.HitCount, item.Key));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/huypq.CacheTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace huypq.CacheTest
+{
+    public class Program
+    {
+        [ProtoBuf.ProtoContract]
+        public class TestData
+        {
+            [ProtoBuf.ProtoMember(1)]
+            public string Name { get; set; }
+        }
+
+        public static void Main(string[] args)
+        {
+            string info;
+            huypq.Cache.RamCache<string, TestData>.Instance.Init(5);
+            info = huypq.Cache.RamCache<string, TestData>.Instance.GetCacheInfo();
+            Console.WriteLine(info);
+
+            Add("data1", "huy");
+            Add("data1", "heo");
+            Add("k1", "heo");
+            Add("k2", "heo");
+            Add("k3", "heo");
+            Add("k4", "heo");
+            Add("data1", "ga2");
+            Add("k5", "heo");
+            Add("k6", "heo");
+
+            Console.Read();
+        }
+
+        static void Add(string key, string name)
+        {
+            huypq.Cache.RamCache<string, TestData>.Instance.AddOrUpdate(key, new TestData() { Name = name });
+            var text = huypq.Cache.RamCache<string, TestData>.Instance.Get(key).Name;
+            Console.WriteLine(text);
+            var info = huypq.Cache.RamCache<string, TestData>.Instance.GetCacheInfo();
+            Console.WriteLine(info);
+        }
+    }
+}
